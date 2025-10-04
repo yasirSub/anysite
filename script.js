5c2257845ce127077b1c6f2d72c9f57257269344
Yasir Subhani <54989470+yasirSub@users.noreply.github.com>
2025-10-04 12:15:41 +0530

    Add script.js

---
+++ script.js
@@ -0,0 +1,14 @@
+function goToFeatures() {
+    // Navigate to features page
+    window.location.href = 'features.html';
+}
+
+function goToHome() {
+    // Navigate back to home page
+    window.location.href = 'index.html';
+}
+
+function openOfferLetter() {
+    // Navigate to offer letter creation page
+    window.location.href = 'offer-letter/index.html';
+}
